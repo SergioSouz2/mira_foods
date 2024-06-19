2e1183d8a3e46d394d0e4848677414b8ca672d16
Sergio Souz2 <sergio.tbl0123@gmail.com>
2024-06-19 03:50:08 -0300

    ✨ feat: Add haeder component

---
+++ lib/widgets/header/header_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.only( left: 20, right: 20, top: 30),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Food Fleet', 
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+                fontFamily: 'Poppins',
+                color: Colors.red
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.menu),
+              color: Colors.grey[600],
+              onPressed: () {},
+            ),
+          ],
+        ),
+    );
+    
+  }
+}
